f4d0cdb2af51b50b941d3b475ad7e8be1ab2fb29
shuklaprakhar <shuklaprakhar@gmail.com>
2024-01-03 19:31:00 -0800

    Add files via upload

---
+++ aichatbotv1.py
@@ -0,0 +1,25 @@
+import openai
+import gradio as gr
+
+openai.api_key = "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"
+
+messages = [
+    {"role": "system", "content": "You are an Exercise Posture Assistant Bot specialized in physical therapy, guiding and supporting you through proper exercise postures."},
+]
+
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+iface = gr.Interface(fn=chatbot, inputs=gr.Textbox(lines=7, label="Chat with Physio AI"), 
+                     outputs=gr.Textbox(label="Reply"), 
+                     title="AI Chatbot",
+                     description="Ask anything you want",
+                     theme="compact")
+iface.launch(share=True)
